743daa46eca355295d023f061e18d3c2a1edec53
dzcrystalcut <dz@crystal-cut.com>
2022-05-26 21:22:09 +0300

    g220526

---
+++ app/src/main/java/com/comhub/dzpermissions/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.comhub.dzpermissions
+
+import android.Manifest
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.comhub.dzpermissions.ui.theme.DzpermissionsTheme
+
+
+@ExperimentalPermissionsApi
+class MainActivity : ComponentActivity() {
+
+    companion object {
+        var assistBitmap: Bitmap? = null
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DzpermissionsTheme {
+                PermissionManager()
+            } //theme
+        }
+    }
+}
